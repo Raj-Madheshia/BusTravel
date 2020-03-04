19cded5a7532ea0918268fd4b67947cfc47446b1
Raj-Madheshia <rajsmadheshia@gmail.com>
2020-03-04 23:29:34 +0530

    OptionActivity Design

---
+++ app/src/main/java/com/example/bustravel/OptionActivity.java
@@ -0,0 +1,14 @@
+package com.example.bustravel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OptionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_option);
+    }
+}
